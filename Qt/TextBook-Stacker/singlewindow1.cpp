fa05ebcc9732879bb70d90634d08bb4b121b6fca
kpeng66 <kpeng66@gmail.com>
2020-02-12 20:18:45 -0800

    Add files via upload

---
+++ Qt/TextBook-Stacker/singlewindow1.cpp
@@ -0,0 +1,14 @@
+#include "singlewindow1.h"
+#include "ui_singlewindow1.h"
+
+singlewindow1::singlewindow1(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::singlewindow1)
+{
+    ui->setupUi(this);
+}
+
+singlewindow1::~singlewindow1()
+{
+    delete ui;
+}
---
+++ Qt/TextBook-Stacker/singlewindow1.h
@@ -0,0 +1,22 @@
+#ifndef SINGLEWINDOW1_H
+#define SINGLEWINDOW1_H
+
+#include <QDialog>
+
+namespace Ui {
+class singlewindow1;
+}
+
+class singlewindow1 : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit singlewindow1(QWidget *parent = nullptr);
+    ~singlewindow1();
+
+private:
+    Ui::singlewindow1 *ui;
+};
+
+#endif // SINGLEWINDOW1_H
