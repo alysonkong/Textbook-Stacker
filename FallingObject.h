b4e2ebc8f31d8d42e6ea2c8df3440b43800784f6
lucasleesf <60523159+lucasleesf@users.noreply.github.com>
2020-02-19 17:11:25 -0800

    Add files via upload

---
+++ FallingObject.h
@@ -0,0 +1,22 @@
+#ifndef FALLINGOBJECT_H
+#define FALLINGOBJECT_H
+
+/*
+
+class FallingObject : public {
+protected:
+    int x_val;
+    int y_val;
+    int fall_rate;
+    QRectF sprite;
+    bool on_stack;
+    bool on_ground;
+public:
+    FallingObject();
+    ~FallingObject();
+    void fall();
+};
+
+ */
+
+#endif // FALLINGOBJECT_H
