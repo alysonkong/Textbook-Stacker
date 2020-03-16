6c1e537d49da7f4477b13d2dbedf97c1288140ce
Katie Tang <katietang@katies-mbp-3.attlocal.net>
2020-03-16 11:52:31 -0700

    cleaned and commented recipe class

--- Qt/TextBook-Stacker/recipe.h
+++ Qt/TextBook-Stacker/recipe.h
@@ -7,7 +7,6 @@
 #include <QPixmap>
 #include <QGridLayout>
 #include <QGraphicsItem>
-#include <string>
 #include <QMediaPlayer>
 
 namespace Recipe {
@@ -21,30 +20,52 @@ class Recipe : public QWidget {
     Q_OBJECT
 
 public:
- /**
- *@brief the recipe constructor randomly generates a list of how many textbooks the user needs to catch
- */
+    /**
+    *@brief the recipe constructor randomly generates a list of how many textbooks the user needs to catch
+    */
     Recipe();
 
- /**
-  *@brief the function updates the recipe display, showing how many of each type of textbook the user needs to catch
- */
+    /**
+    *@brief the function updates the recipe display, showing how many of each type of textbook the user needs to catch
+    * @param code the book code of the book that was correctly caught
+    */
     void update_recipe_display(int code);
 
-public slots:
-    void book_caught(int code);
+    /**
+     * @brief newrecipe generates and displays a new recipe, replacing the old one
+     * @param layout is the layout of the recipe display prior to the recipe getting updated
+     */
     void newrecipe(QLayout* layout);
 
-
+public slots:
+    /**
+     * @brief book_caught receives a signal from gameview sending the type of book caught by the avatar
+     * @param code is the type of book caught
+     */
+    void book_caught(int code);
 
 signals:
+    /**
+     * @brief round_complete sends signal out that the user has caught all of the necessary books in the round
+     */
     void round_complete(int);
+
+    /**
+     * @brief wrong_book sends out signal that the avatar has caught the wrong book
+     */
     void wrong_book();
-    void updatescore(int); //the int here is gonna be the book code, not points added
+
+    /**
+     * @brief updatescore sends out signal that the score should be updated since a correct book was caught
+     * @param int the number indicating which textbook was caught
+     */
+    void updatescore(int);
 
 private:
-    void display_recipe(); //should it be void????
-
+    /**
+     * @brief display_recipe displays the recipe, showing the user how many of each type of book they need to catch
+     */
+    void display_recipe();
 
     int bigcplusplus_count = 0;
     int bio_count = 0;
@@ -52,17 +73,14 @@ private:
     int soc_count = 0;
     int eng_count = 0;
     int philo_count = 0;
-
+    int total_books = 0;
     QLabel* bigc_count_display;
     QLabel *bio_count_display;
     QLabel *chem_count_display;
     QLabel *soc_count_display;
     QLabel *eng_count_display;
     QLabel *philo_count_display;
-
     QGridLayout *layout;
-
-    int total_books = 0;
     QFont f;
     QMediaPlayer* stacking_sound;
     QMediaPlayer* error_sound;
